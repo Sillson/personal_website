8ae5b14e2f1da1e41668fdc2b6e675c08d70fdfc
Stuart Illson <sillson88@gmail.com>
2018-05-13 23:39:43 -0700

    sworkit

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/portfolio/hookes',          to: 'portfolio#hookes'
   get '/portfolio/passive-capture', to: 'portfolio#passive_capture'
   get '/portfolio/tensor-flow', to: 'portfolio#tensor_flow'
+  get '/portfolio/sworkit', to: 'portfolio#sworkit'
 end
